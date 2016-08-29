8727f57fc2f128ec6be62fe4417ebb48346d3769
Pavel Prischepa <pprischepa@gmail.com>
2016-08-29 18:21:18 +0700

    Updated

--- run_nfs.sh
+++ run_nfs.sh
@@ -35,11 +35,11 @@ function start()
 
     # -N 4.x: disable NFSv4
     # -V 3: enable NFSv3
-    /usr/sbin/rpc.mountd -N 2 -V 3 -N 4 -N 4.1
+    /usr/sbin/rpc.mountd -N 2 -N 3 -V 4 -V 4.1
 
     /usr/sbin/exportfs -r
     # -G 10 to reduce grace time to 10 seconds (the lowest allowed)
-    /usr/sbin/rpc.nfsd -G 10 -N 2 -V 3 -N 4 -N 4.1 2
+    /usr/sbin/rpc.nfsd -G 10 -N 2 -N 3 -V 4 -V 4.1 2
     /usr/sbin/rpc.statd --no-notify
     echo "NFS started"
 }
